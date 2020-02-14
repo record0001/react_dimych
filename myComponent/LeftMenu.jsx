488c907e83855984f0bd50d614920fe9f22018de
record0001 <record0001@gmail.com>
2020-02-15 00:09:41 +0300

    Обновление ссылок и топ меню

--- myComponent/LeftMenu.jsx
+++ myComponent/LeftMenu.jsx
@@ -8,11 +8,11 @@ return (
 
 <div className={left_menu.LeftMenu}>
 
-<div><a href="#">Главная</a></div>
-<div><a href="#">Игры</a></div>
-<div><a href="#">Фото</a></div>
-<div><a href="#">Фильмы</a></div>
-<div><a href="#">Развлечения</a></div>
+<div><a href="/#">Главная</a></div>
+<div><a href="/#">Игры</a></div>
+<div><a href="/#">Фото</a></div>
+<div><a href="/#">Фильмы</a></div>
+<div><a href="/#">Развлечения</a></div>
 
 </div>
 
--- myComponent/TopMenu.jsx
+++ myComponent/TopMenu.jsx
@@ -10,11 +10,11 @@ return (
     <div className={top_menu.TopMenu}>
         
 
-            <div><a href="#">TopMenu</a></div>
-            <div><a href="#">User ico</a></div>
-            <div><a href="#">User name</a></div>
-            <div><a href="#">User status</a></div>
-            <div><a href="#">Вход/Регистрация</a></div>
+            <div><a href="/#">TopMenu</a></div>
+            <div><a href="/#">User ico</a></div>
+            <div><a href="/#">User name</a></div>
+            <div><a href="/#">User status</a></div>
+            <div><a href="/#">Вход/Регистрация</a></div>
         
 
 
